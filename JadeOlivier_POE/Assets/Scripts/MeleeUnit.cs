b44ad71f73a7b85200412593d898fbe646180e71
JadeyOlivier <55444202+JadeyOlivier@users.noreply.github.com>
2019-10-14 12:36:03 +0200

    AdamTeachingUs

---
+++ JadeOlivier_POE/Assets/Scripts/MeleeUnit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeUnit : Unit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Random team, x and y
+        x = Random.Range(0, 20); //Unity random
+        hp = 10;
+        maxHP = hp;
+        attk = 2;
+        range = 1;
+
+        team = Random.Range(0, 2);
+        GetComponent<MeshRenderer>().material = mat[team];
+    }
+
+}
---
+++ JadeOlivier_POE/Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    [SerializeField] protected int range;
+    [SerializeField] protected int x;
+    [SerializeField] protected int y;
+    [SerializeField] protected int hp;
+    [SerializeField] protected int maxHP;
+    [SerializeField] protected int attk;
+    [SerializeField] protected int speed;
+    [SerializeField] protected int team;
+    [SerializeField] protected Material[] mat;
+
+    public int X { get => x; set => x = value; }
+    public int Y { get => y; set => y = value; }
+    public int Hp { get => hp; set => hp = value; }
+    public int MaxHP { get => maxHP;}
+    public int Attk { get => attk;}
+    public int Speed { get => speed;}
+    public int Range { get => range;}
+    public int Team { get => team;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
