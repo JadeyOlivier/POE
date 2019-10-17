2044a8976e1733a4049ce8eaff9ca1c65aaae873
JadeyOlivier <55444202+JadeyOlivier@users.noreply.github.com>
2019-10-17 10:42:15 +0200

    Added wizards

---
+++ JadeOlivier_POE/Assets/Scripts/WizardUnit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardUnit : Unit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 15;
+        maxHP = hp;
+        attk = 3;
+        range = 1;
+        speed = 0.25f;
+
+        team = 2;
+        GetComponent<MeshRenderer>().material = mat[team];
+        gameObject.tag = "Wizard";
+    }
+
+
+}
